7b3a0c29312cf460f5153b6fa98a52ffcddddb01
sharma8910 <sonu9123641936@gmail.com>
2026-06-10 13:05:41 +0530

    fix cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://io-social-app-frontend.vercel.app/'], credentials: true
 }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
@@ -36,7 +36,7 @@ app.get("/",(req,res)=>{
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://io-social-app-frontend.vercel.app/"],
     methods: ["GET", "POST"]
   }
 });
@@ -83,6 +83,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000,() => {
-  console.log('http://localhost:5000')
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,() => {
+  console.log(`Server running on port ${PORT}`)
 });
